97e83dea0fc1e392d1a044a271cca398853b2a77
ogata_n <mgtnao5230@gmail.com>
2021-10-01 11:33:12 +0900

    ファイル整理

--- daywork/1001/js/object.js
+++ daywork/1001/js/object.js
@@ -29,6 +29,32 @@
 // console.log(cnum)
 // console.log(cnum[1])
 
+
+// //befor
+// const janken = ['ぐー','ちょき','ぱー']
+// const result = ['user1の勝利です','user2の勝利です','あいこです']
+
+// const user1 = janken[Math.floor(Math.random() * janken.length)]
+// const user2 = janken[Math.floor(Math.random() * janken.length)]
+
+// console.log('user1:',user1,'  user2:',user2)
+
+// if(user1 === 'ぐー'){
+//     if(user2 === 'ぐー'){console.log(result[2])}
+//     else if(user2 === 'ちょき'){console.log(result[1])}
+//     else if(user2 === 'ぱー'){console.log(result[0])}
+// }else if(user1 === 'ちょき'){
+//     if(user2 === 'ぐー'){console.log(result[1])}
+//     else if(user2 === 'ちょき'){console.log(result[2])}
+//     else if(user2 === 'ぱー'){console.log(result[0])}
+// }else if(user1 === 'ぱー'){
+//     if(user2 === 'ぐー'){console.log(result[0])}
+//     else if(user2 === 'ちょき'){console.log(result[1])}
+//     else if(user2 === 'ぱー'){console.log(result[2])}
+// }else{console.log('eller')}
+
+
+//After
 const janken = ['ぐー','ちょき','ぱー']
 
 const user1 = janken[Math.floor(Math.random() * janken.length)]
