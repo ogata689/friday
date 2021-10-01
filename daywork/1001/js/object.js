80cd91482ffddc3b6a2363f59705dc92dffbb369
ogata_n <mgtnao5230@gmail.com>
2021-10-01 12:46:54 +0900

    js練習問題_回答

--- daywork/1001/js/object.js
+++ daywork/1001/js/object.js
@@ -54,20 +54,70 @@
 // }else{console.log('eller')}
 
 
-//After
-const janken = ['ぐー','ちょき','ぱー']
+// //After
+// const janken = ['ぐー','ちょき','ぱー']
 
-const user1 = janken[Math.floor(Math.random() * janken.length)]
-const user2 = janken[Math.floor(Math.random() * janken.length)]
+// const user1 = janken[Math.floor(Math.random() * janken.length)]
+// const user2 = janken[Math.floor(Math.random() * janken.length)]
 
-console.log('user1:',user1,'  user2:',user2)
+// console.log('user1:',user1,'  user2:',user2)
 
-if(user1 === user2){
-    console.log('あいこです')
-}else if(
-    user1 === 'ぐー' && user2 === 'ちょき' ||
-    user1 === 'ちょき' && user2 === 'ぱー' ||
-    user1 === 'ぱー' && user2 === 'ぐー'
-){
-    console.log('user1の勝利です')
-}else{console.log('user2の勝利です')}
+// if(user1 === user2){
+//     console.log('あいこです')
+// }else if(
+//     user1 === 'ぐー' && user2 === 'ちょき' ||
+//     user1 === 'ちょき' && user2 === 'ぱー' ||
+//     user1 === 'ぱー' && user2 === 'ぐー'
+// ){
+//     console.log('user1の勝利です')
+// }else{console.log('user2の勝利です')}
+
+
+// //js問題
+
+//Q1
+const num1to50 = []
+const num50to1 = []
+
+for(i = 0; i <= 50; i++){
+    num1to50.push(i)
+    num50to1.push(50 - i)
+}
+
+console.log(num1to50)
+console.log(num50to1)
+
+
+//Q2
+const fruits  = ['りんご', 'バナナ', 'パイナップル', 'ぶどう', 'すいか', 'めろん'];
+
+for(key in fruits){
+    console.log(fruits[key])
+}
+
+
+//Q3
+const fruits2 = {
+    red : ['りんご', 'さくらんぼ', 'いちご'],
+    orange : ['かき', 'みかん', 'びわ'],
+    yellow : ['バナナ', 'パイナップル', 'レモン']
+};
+
+for(color in fruits2){
+    console.log(color,'(',fruits2[color][0],'・',fruits2[color][1],'・',fruits2[color][2],')');
+}
+
+//Q4
+const num = {};
+num.even = [];
+num.odd = [];
+
+for(i = 0; i <= 100; i++){
+    if(i % 2 === 0 || i === 0){
+        num.even.push(i)
+    }else{
+        num.odd.push(i)
+    }
+}
+
+console.log(num)
