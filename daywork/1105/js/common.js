82bd4242d2650600946f6c0ea897f3f11a23179f
ogata_n <mgtnao5230@gmail.com>
2021-11-05 10:38:26 +0900

    for文の修正

--- daywork/1105/js/common.js
+++ daywork/1105/js/common.js
@@ -5,63 +5,57 @@ console.log(luck[rand]);
 
 //Q1 - 2
 function omikuzi(){
-    const luck = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶'];
-    const rand = Math.floor(Math.random() * luck.length);
-    return luck[rand];
+  const luck = ['大吉', '中吉', '小吉', '吉', '末吉', '凶', '大凶'];
+  const rand = Math.floor(Math.random() * luck.length);
+  return luck[rand];
 }
 
 //Q2
-function calcTheNumbersInArray(array){
-    let ans = 0;
-    for(num in array){
-        ans += array[num];
-    }
-    return ans
+function calcTheNumbersInArray(numbers){
+  let ans = 0;
+  numbers.forEach(function(element){ans += element})
+  return ans
 }
 
 //Q3
 const users = [
-    {
-      name: '佐藤',
-      score: 90
-    },
-    {
-      name: '山田',
-      score: 40
-    },
-    {
-      name: '斎藤',
-      score: 70
-    },
-    {
-      name: '高橋',
-      score: 30
-    },
-    {
-      name: '田中',
-      score: 80
-    }
+  {
+    name: '佐藤',
+    score: 90
+  },
+  {
+    name: '山田',
+    score: 40
+  },
+  {
+    name: '斎藤',
+    score: 70
+  },
+  {
+    name: '高橋',
+    score: 30
+  },
+  {
+    name: '田中',
+    score: 80
+  }
 ];
 
-function resultantOfStudy(array,num){
-    let average = 0;
-    let result = '';
-    for(let stat in array){
-        average += array[stat]['score'];
-    }
-    average = average / array.length
+function resultantOfStudy(users,num){
+  let average = 0;
+  let result = '';
+  users.forEach(function(element){average += element['score']});
+  average = average / users.length
 
-    if(array[num]['score'] > average){
-        result = (array[num]['name'] + 'は' + array[num]['score'] + 'で合格です。');
-    }else{
-        result = (array[num]['name'] + 'は' + array[num]['score'] + 'で失格です。');
-    };
+  if(users[num]['score'] > average){
+    result = (users[num]['name'] + 'は' + users[num]['score'] + 'で合格です。');
+  }else{
+    result = (users[num]['name'] + 'は' + users[num]['score'] + 'で失格です。');
+  };
 
-    return result;
+  return result;
 }
 
-console.log(resultantOfStudy(users,0));
-console.log(resultantOfStudy(users,1));
-console.log(resultantOfStudy(users,2));
-console.log(resultantOfStudy(users,3));
-console.log(resultantOfStudy(users,4));
+for(let num in users){
+  console.log(resultantOfStudy(users,num));
+}
